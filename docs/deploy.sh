41be6a6693a5c022f71867bbd673d67cdc7fec7e
Qian Liu <qliu7@buffalo.edu>
2020-11-12 20:31:48 -0500

    deploy

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/rworkflow/rworkflow master:gh-pages
+git push -f https://github.com/rworkflow/rworkflow.github.io master:main
 
 cd -
